2c1f327c4c130333fee6368b872283ea96ff8596
Hasnat Mosharraf Sami <mosharrafhasnat@gmail.com>
2025-01-26 12:28:31 +0100

    Hello World

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,11 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    print ("Hello")
+    name = input("What is your name?")
+    print ("Nice to meet you")
 
+
+if __name__ == "__main__":
+    main()
